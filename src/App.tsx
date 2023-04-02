dd1392aa1395dbcb73326174142337636e9c1b09
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-02 15:22:33 +0545

    timer tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer, useRef } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialState = {
+  count: 0,
+  isRunning: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "start":
+      return { ...state, isRunning: true };
+    case "stop":
+      return { ...state, isRunning: false };
+    case "reset":
+      return { count: 0, isRunning: false };
+    case "tick":
+      return { ...state, count: state.count + 1 };
+    default:
+      throw new Error();
+  }
+};
+
+function Timer() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const intervalRef = useRef();
+
+  const startTimer = () => {
+    dispatch({ type: "start" });
+    intervalRef.current = setInterval(() => {
+      dispatch({ type: "tick" });
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    dispatch({ type: "stop" });
+    clearInterval(intervalRef.current);
+  };
+
+  const resetTimer = () => {
+    dispatch({ type: "reset" });
+    clearInterval(intervalRef.current);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="timer-container">
+      <h1>Simple Timer</h1>
+      <div className="timer-display">{state.count}</div>
+      <div className="timer-controls">
+        {!state.isRunning && <button onClick={startTimer}>Start</button>}
+        {state.isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default Timer;
